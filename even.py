6dfd3a1fee2fc8ff6722398c2547f5ab540b6892
Akhila <vemuriakhilacse@gamil.com>
2024-03-02 21:07:52 +0530

    updated

---
+++ even.py
@@ -0,0 +1,6 @@
+a=input("enter a number:")
+b=int(a)
+if b%2==0:
+    print("Even number")
+else:
+    print("Not a even number")
---
+++ odd.py
@@ -0,0 +1,5 @@
+a=int(input("Enter a number:"))
+if a!=0:
+    print("Odd number")
+else:
+    print("Not an odd number")
