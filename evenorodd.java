0900954de823306bdb069fe7310f2aa73db50757
Akhila <akhilavemuri83@gmail.com>
2025-08-21 10:57:44 +0530

    commit

---
+++ evenorodd.java
@@ -0,0 +1,15 @@
+class evenorodd
+{
+public static void main(String[ ] args)
+{
+int n=67;
+if (n%2==0)
+{
+System.out.println("even number");
+}
+else
+{
+System.out.println("odd number");
+}
+}
+}
